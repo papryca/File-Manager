2e06c10cd1950363370abd8fb9944bda49929d84
inessa <inessasmorgunova@gmail.com>
2024-10-13 23:37:23 -0400

    feat: add add.js

---
+++ commands/add.js
@@ -0,0 +1,24 @@
+import {Command} from "./command.js";
+import path from "path";
+import fs from "fs/promises";
+
+export class Add extends Command {
+    async validate(app, args) {
+        if (args.getArgs().length !== 1) {
+            throw new Error('Invalid argument count');
+        }
+        const filePath = path.join(app.getCurrentPath(), args.getIndexArg(0));
+        try {
+            await fs.access(filePath);
+        } catch (e){
+            return;
+        }
+        throw new Error('File already exists')
+    }
+    async execute(app, args) {
+        const filePath = path.join(app.getCurrentPath(), args.getIndexArg(0));
+        const fileContent = '';
+        await fs.writeFile(filePath, fileContent, 'utf8');
+
+    }
+}
