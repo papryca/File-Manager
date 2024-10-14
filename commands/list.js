7dd2d58fe88cfcdd51827f9c4c99fcf25e6c84f7
inessa <inessasmorgunova@gmail.com>
2024-10-13 23:40:57 -0400

    feat: add list logic

--- commands/list.js
+++ commands/list.js
@@ -7,6 +7,7 @@ export class List extends Command {
     async execute(app, args) {
         const files = await fs.readdir(app.getCurrentPath());
         const data = [];
+
         for (const file of files) {
             const filePath = path.join(app.getCurrentPath(),file);
             const stats =  await fs.lstat(filePath);
@@ -15,6 +16,12 @@ export class List extends Command {
                 Type: stats.isFile() ? 'File' : 'Directory'
             })
         }
+        data.sort((a, b) => {
+            if (a.Type === b.Type) {
+                return a.Name.localeCompare(b.Name);
+            }
+            return a.Type === 'Directory' ? -1 : 1;
+        });
         console.table(data);
     }
 }
