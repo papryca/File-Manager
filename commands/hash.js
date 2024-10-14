c141f30f90ef7d605981aa7e251db90fed2ad0a8
inessa <inessasmorgunova@gmail.com>
2024-10-13 23:38:46 -0400

    feat: add hash

---
+++ commands/hash.js
@@ -0,0 +1,34 @@
+import {Command} from "./command.js";
+import path from "path";
+import fs from "fs/promises";
+import {createHash} from "crypto";
+import {createReadStream} from "fs";
+
+export class Hash extends Command {
+    async validate(app, args) {
+        if (args.getArgs().length !== 1) {
+            throw new Error('Invalid argument count');
+        }
+        const filePath = path.join(app.getCurrentPath(), args.getIndexArg(0));
+
+        const stat  = await fs.stat(filePath)
+        if (!stat.isFile()) {
+            throw new Error('Cannot count hash for directory');
+        }
+
+        await fs.access(filePath);
+
+    }
+    async execute(app, args) {
+        const filePath = path.join(app.getCurrentPath(), args.getIndexArg(0));
+        const hashSum = createHash('SHA256');
+        const readStream = createReadStream(filePath);
+        readStream.on('data', (chunk) => {
+            hashSum.update(chunk);
+        });
+        readStream.on('end', () => {
+            console.log(hashSum.digest('hex'));
+        });
+
+    }
+}
