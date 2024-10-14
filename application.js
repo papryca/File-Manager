1250aae36e7ab16f9cdbe29fe9b126595b245f9d
inessa <inessasmorgunova@gmail.com>
2024-10-13 23:41:28 -0400

    feat: add commands

--- application.js
+++ application.js
@@ -3,6 +3,18 @@ import {ChangeDirectory} from "./commands/changeDirectory.js";
 import {List} from "./commands/list.js";
 import {Arguments} from "./arguments.js";
 import {Up} from "./commands/up.js";
+import {Cat} from "./commands/cat.js";
+import {Add} from "./commands/add.js";
+import {Rn} from "./commands/rn.js";
+import {Cp} from "./commands/cp.js";
+import {Mv} from "./commands/mv.js";
+import {Rm} from "./commands/rm.js";
+import {Hash} from "./commands/hash.js";
+import {Compress} from "./commands/compress.js";
+import {Decompress} from "./commands/decompress.js";
+import {Os} from "./commands/os.js";
+
+
 
 export class Application {
     constructor() {
@@ -12,6 +24,16 @@ export class Application {
             'cd': new ChangeDirectory(),
             'ls': new List(),
             'up': new Up(),
+            'cat': new Cat(),
+            'add': new Add(),
+            'rn': new Rn(),
+            'cp': new Cp(),
+            'mv': new Mv(),
+            'rm': new Rm(),
+            'hash': new Hash(),
+            'compress': new Compress(),
+            'decompress': new Decompress(),
+            'os': new Os(),
         }
     }
 
@@ -20,6 +42,7 @@ export class Application {
 
         const userName = args.getNamed('username');
         console.log(`Welcome to the File Manager, ${userName}!`);
+        console.log(`You are currently in ${this.getCurrentPath()}`);
 
         function exitFunc(name) {
             console.log(`Thank you for using File Manager, ${name}, goodbye!`);
@@ -36,22 +59,29 @@ export class Application {
                 exitFunc(userName);
             }
 
-            let call = input.split(' ');
-            if (call[0] in this.commands) {
-                let argString = '';
-                if (call[1]) {
-                    argString = call[1]
-                }
-                const callArgs = new Arguments(argString.split(' '))
-                try{
-                    await this.commands[call[0]].validate(this, callArgs)
-                    await this.commands[call[0]].execute(this, callArgs)
-                } catch (e) {
-                    console.log(e.message);
+            const call = input.split(' ');
+            const command = call.shift();
+            if (command in this.commands) {
+                const callArgs = new Arguments(call)
+                try {
+                    await this.commands[command].validate(this, callArgs)
+
+                    try {
+                        await this.commands[command].execute(this, callArgs)
+                    }catch(e) {
+                        console.log('Operation failed')
+                        console.log(e.message)
+                    }
+                }catch(e) {
+                    console.log('Invalid input')
+                    console.log(e.message)
                 }
 
+
+
+                console.log(`You are currently in ${this.getCurrentPath()}`);
             } else {
-                console.log('NO such command')
+                console.log('No such command')
             }
         })
     }
